e13b10f78b7a10aa7f2d9b693de856c86d38266d
Maurits van der Schee <maurits@vdschee.nl>
2022-12-04 09:18:57 +0100

    day04

---
+++ day04/part2/Program.vb
@@ -0,0 +1,33 @@
+Imports System.IO
+
+Module Program
+
+    Sub Main()
+
+        Dim input = File.ReadAllText("input")
+        Dim pairs = input.Trim().Split(vbLf)
+        Dim count = 0
+
+        For Each pair In pairs
+
+            Dim ranges = pair.Split(",")
+            Dim range1 = ranges(0).Split("-")
+            Dim range2 = ranges(1).Split("-")
+            Dim r1min = Integer.Parse(range1(0))
+            Dim r1max = Integer.Parse(range1(1))
+            Dim r2min = Integer.Parse(range2(0))
+            Dim r2max = Integer.Parse(range2(1))
+
+            If (r1max >= r2min And r1min <= r2max) Or (r2max >= r1min And r2min <= r1max) Then
+
+                count += 1
+
+            End If
+
+        Next
+
+        Console.WriteLine(count)
+
+    End Sub
+  
+End Module
