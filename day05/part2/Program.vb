390482c83005d73f47ae24f7a09fee94f8e3bb6b
Maurits van der Schee <maurits@vdschee.nl>
2022-12-05 15:24:52 +0100

    day05

--- day05/part2/Program.vb
+++ day05/part2/Program.vb
@@ -1,28 +1,27 @@
 Imports System.IO
 
 Module Program
-
     Sub Main()
 
         Dim input = File.ReadAllText("input")
         Dim parts = input.Split(vbLf & vbLf)
         Dim lines = parts(0).Split(vbLf)
-        Dim columns = (lines(0).Length() + 1) / 4
+        Dim columns = (lines(0).Length() + 1)/4
 
         Dim stacks(columns) as Stack
-        For i = 0 To columns-1
+        For i = 0 To columns - 1
             stacks(i) = new Stack()
         Next i
 
         For Each line In lines
 
             If line = "" Then
-                Exit For                
+                Exit For
             End If
 
-            For i = 0 To columns-1
+            For i = 0 To columns - 1
 
-                Dim letter = line.Substring(i*4+1,1)
+                Dim letter = line.Substring(i*4 + 1, 1)
 
                 If Asc(letter) >= Asc("A") And Asc(letter) <= Asc("Z") Then
                     stacks(i).Push(letter)
@@ -34,13 +33,13 @@ Module Program
         Next
 
         Dim reverse(columns) as Stack
-        For i = 0 To columns-1
-    
+        For i = 0 To columns - 1
+
             reverse(i) = new Stack()
-            Do While stacks(i).Count()>0
+            Do While stacks(i).Count() > 0
                 reverse(i).Push(stacks(i).Pop())
-            Loop            
-    
+            Loop
+
         Next i
         stacks = reverse
 
@@ -49,26 +48,24 @@ Module Program
 
             Dim words = line.Split(" ")
             Dim count = Integer.Parse(words(1))
-            Dim source = Integer.Parse(words(3))-1
-            Dim destination = Integer.Parse(words(5))-1
+            Dim source = Integer.Parse(words(3)) - 1
+            Dim destination = Integer.Parse(words(5)) - 1
 
             Dim temp = new Stack()
-            For i = 0 To count-1
+            For i = 0 To count - 1
                 temp.Push(stacks(source).Pop())
             Next i
-            For i = 0 To count-1
+            For i = 0 To count - 1
                 stacks(destination).Push(temp.Pop())
             Next i
 
         Next
 
         Dim solution = ""
-        For i = 0 To columns-1
+        For i = 0 To columns - 1
             solution &= stacks(i).Pop()
         Next i
 
         Console.WriteLine(solution)
-
     End Sub
-  
 End Module
