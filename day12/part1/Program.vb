615908330d188422788cf2ff22460b0405068d45
Maurits van der Schee <maurits@vdschee.nl>
2022-12-12 12:29:40 +0100

    day12

--- day12/part1/Program.vb
+++ day12/part1/Program.vb
@@ -4,9 +4,10 @@ Module Program
 
     Sub Main()
 
-        Dim input = File.ReadAllText("input")
+        Dim input = File.ReadAllText("input.test")
         Dim lines = input.Trim().Replace(vbCrLf, vbLf).Split(vbLf)
         Dim fields = New Dictionary(Of (Integer, Integer), Integer)()
+        Dim distances = New Dictionary(Of (Integer, Integer), Integer)()
         Dim width = lines(0).Length
         Dim height = lines.Length
         Dim start As (x As Integer, y As Integer) = (0, 0)
@@ -28,15 +29,14 @@ Module Program
             Next x
         Next y
 
-        Dim firstPath As New List(Of (x As Integer, y As Integer))({start})
-        Dim paths As New List(Of List(Of (x As Integer, y As Integer)))({firstPath})
+        Dim frontier As New List(Of (x As Integer, y As Integer))({start})
+        distances(start) = 0
         Dim steps = 1
         Do
             Dim moves() As (x As Integer, y As Integer) = {(0, -1), (0, 1), (-1, 0), (1, 0)}
-            Dim newPaths = New List(Of List(Of (x As Integer, y As Integer)))()
-            For Each path In paths
+            Dim newFrontier = New List(Of (x As Integer, y As Integer))()
+            For Each pos In frontier
                 For Each move In moves
-                    Dim pos = path.Last()
                     Dim newPos As (x As Integer, y As Integer) = (pos.x + move.x, pos.y + move.y)
                     If newPos.x < 0 Or newPos.y < 0 Or newPos.x >= width Or newPos.y >= height Then
                         Continue For
@@ -44,18 +44,16 @@ Module Program
                     If fields(newPos) > fields(pos) + 1 Or fields(newPos) < fields(pos) Then
                         Continue For
                     End If
-                    If path.Contains(newPos) Then
+                    If newFrontier.Contains(newPos) Then
                         Continue For
                     End If
                     If newPos.x = goal.x And newPos.y = goal.y Then
                         Exit Do
                     End If
-                    Dim newPath = path.ToList()
-                    newPath.Add(newPos)
-                    newPaths.Add(newPath)
+                    newFrontier.Add(newPos)
                 Next
             Next
-            paths = newPaths
+            frontier = newFrontier
             steps += 1
         Loop While True
 
