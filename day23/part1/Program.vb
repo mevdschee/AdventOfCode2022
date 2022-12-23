222d8c79775aa4a74f144fed6a8acd3b6c56b6fd
Maurits van der Schee <maurits@vdschee.nl>
2022-12-23 23:42:16 +0100

    day23

--- day23/part1/Program.vb
+++ day23/part1/Program.vb
@@ -1,5 +1,4 @@
 Imports System.IO
-Imports System.Text.RegularExpressions
 
 Module Program
 
@@ -9,29 +8,29 @@ Module Program
         Dim lines = input.Split(vbLf)
         Dim field = New Dictionary(Of (x As Integer, y As Integer), Boolean)()
 
-        For y=0 To lines.Count-1
-            For x=0 To lines(y).Length-1
+        For y = 0 To lines.Count - 1
+            For x = 0 To lines(y).Length - 1
                 If lines(y)(x) = "#" Then
-                    field((x,y)) = True
+                    field((x, y)) = True
                 End If
             Next x
         Next y
 
-        For i=0 To 10-1
+        For i = 0 To 10 - 1
             Dim moves = New Dictionary(Of (x As Integer, y As Integer), List(Of (x As Integer, y As Integer)))()
             For Each kv In field
                 Dim pos = kv.Key
                 Dim newpos = pos
-                For j=0 To 4-1
-                    Dim checkpos = new List(Of (x As Integer, y As Integer))()
-                    checkpos.Add((pos.x-1, pos.y-1))
-                    checkpos.Add((pos.x+0, pos.y-1))
-                    checkpos.Add((pos.x+1, pos.y-1))
-                    checkpos.Add((pos.x-1, pos.y+0))
-                    checkpos.Add((pos.x+1, pos.y+0))
-                    checkpos.Add((pos.x-1, pos.y+1))
-                    checkpos.Add((pos.x+0, pos.y+1))
-                    checkpos.Add((pos.x+1, pos.y+1))
+                For j = 0 To 4 - 1
+                    Dim checkpos = New List(Of (x As Integer, y As Integer))()
+                    checkpos.Add((pos.x - 1, pos.y - 1))
+                    checkpos.Add((pos.x + 0, pos.y - 1))
+                    checkpos.Add((pos.x + 1, pos.y - 1))
+                    checkpos.Add((pos.x - 1, pos.y + 0))
+                    checkpos.Add((pos.x + 1, pos.y + 0))
+                    checkpos.Add((pos.x - 1, pos.y + 1))
+                    checkpos.Add((pos.x + 0, pos.y + 1))
+                    checkpos.Add((pos.x + 1, pos.y + 1))
                     Dim neighbours = 0
                     For Each cp In checkpos
                         If field.ContainsKey(cp) Then
@@ -41,24 +40,24 @@ Module Program
                     If neighbours = 0 Then
                         Exit For
                     End If
-                    checkpos = new List(Of (x As Integer, y As Integer))()
-                    Select (i+j) Mod 4
+                    checkpos = New List(Of (x As Integer, y As Integer))()
+                    Select Case (i + j) Mod 4
                         Case 0 ' north
-                            checkpos.Add((pos.x-1, pos.y-1))
-                            checkpos.Add((pos.x+0, pos.y-1))
-                            checkpos.Add((pos.x+1, pos.y-1))                            
+                            checkpos.Add((pos.x - 1, pos.y - 1))
+                            checkpos.Add((pos.x + 0, pos.y - 1))
+                            checkpos.Add((pos.x + 1, pos.y - 1))
                         Case 1 ' south
-                            checkpos.Add((pos.x-1, pos.y+1))
-                            checkpos.Add((pos.x+0, pos.y+1))
-                            checkpos.Add((pos.x+1, pos.y+1))                            
+                            checkpos.Add((pos.x - 1, pos.y + 1))
+                            checkpos.Add((pos.x + 0, pos.y + 1))
+                            checkpos.Add((pos.x + 1, pos.y + 1))
                         Case 2 ' west
-                            checkpos.Add((pos.x-1, pos.y-1))
-                            checkpos.Add((pos.x-1, pos.y+0))
-                            checkpos.Add((pos.x-1, pos.y+1))
+                            checkpos.Add((pos.x - 1, pos.y - 1))
+                            checkpos.Add((pos.x - 1, pos.y + 0))
+                            checkpos.Add((pos.x - 1, pos.y + 1))
                         Case 3 ' east
-                            checkpos.Add((pos.x+1, pos.y-1))
-                            checkpos.Add((pos.x+1, pos.y+0))
-                            checkpos.Add((pos.x+1, pos.y+1))
+                            checkpos.Add((pos.x + 1, pos.y - 1))
+                            checkpos.Add((pos.x + 1, pos.y + 0))
+                            checkpos.Add((pos.x + 1, pos.y + 1))
                     End Select
                     neighbours = 0
                     For Each cp In checkpos
@@ -72,7 +71,7 @@ Module Program
                     End If
                 Next j
                 If Not moves.ContainsKey(newpos) Then
-                    moves(newpos) = new List(Of (x As Integer, y As Integer))
+                    moves(newpos) = New List(Of (x As Integer, y As Integer))
                 End If
                 moves(newpos).Add(pos)
             Next
@@ -83,30 +82,30 @@ Module Program
                 Else
                     For Each prevpos In kv.Value
                         newfield(prevpos) = True
-                    Next                    
+                    Next
                 End If
             Next
             field = newfield
         Next i
 
-        Dim min As (x As Integer, y As Integer) = (0,0)
-        Dim max As (x As Integer, y As Integer) = (0,0)
+        Dim min As (x As Integer, y As Integer) = (0, 0)
+        Dim max As (x As Integer, y As Integer) = (0, 0)
         For Each kv In field
-            min.x = Math.Min(min.x,kv.Key.x)
-            min.y = Math.Min(min.y,kv.Key.y)
-            max.x = Math.Max(max.x,kv.Key.x)
-            max.y = Math.Max(max.y,kv.Key.y)
+            min.x = Math.Min(min.x, kv.Key.x)
+            min.y = Math.Min(min.y, kv.Key.y)
+            max.x = Math.Max(max.x, kv.Key.x)
+            max.y = Math.Max(max.y, kv.Key.y)
         Next
 
         Dim sum = 0
-        For y=min.y To max.y
-            For x=min.x To max.x
-                If Not field.ContainsKey((x,y)) Then
+        For y = min.y To max.y
+            For x = min.x To max.x
+                If Not field.ContainsKey((x, y)) Then
                     sum += 1
                 End If
             Next x
-        Next y        
-        
+        Next y
+
         Console.WriteLine(sum)
 
     End Sub
--- day23/part2/Program.vb
+++ day23/part2/Program.vb
@@ -1,5 +1,4 @@
 Imports System.IO
-Imports System.Text.RegularExpressions
 
 Module Program
 
@@ -9,31 +8,32 @@ Module Program
         Dim lines = input.Split(vbLf)
         Dim field = New Dictionary(Of (x As Integer, y As Integer), Boolean)()
 
-        For y=0 To lines.Count-1
-            For x=0 To lines(y).Length-1
+        For y = 0 To lines.Count - 1
+            For x = 0 To lines(y).Length - 1
                 If lines(y)(x) = "#" Then
-                    field((x,y)) = True
+                    field((x, y)) = True
                 End If
             Next x
         Next y
 
         Dim round = 0
-        For i=0 To 1000000-1
+        For i = 0 To 1000000 - 1
             Dim stayed = 0
             Dim moves = New Dictionary(Of (x As Integer, y As Integer), List(Of (x As Integer, y As Integer)))()
             For Each kv In field
                 Dim pos = kv.Key
                 Dim newpos = pos
-                For j=0 To 4-1
-                    Dim checkpos = new List(Of (x As Integer, y As Integer))()
-                    checkpos.Add((pos.x-1, pos.y-1))
-                    checkpos.Add((pos.x+0, pos.y-1))
-                    checkpos.Add((pos.x+1, pos.y-1))
-                    checkpos.Add((pos.x-1, pos.y+0))
-                    checkpos.Add((pos.x+1, pos.y+0))
-                    checkpos.Add((pos.x-1, pos.y+1))
-                    checkpos.Add((pos.x+0, pos.y+1))
-                    checkpos.Add((pos.x+1, pos.y+1))
+                For j = 0 To 4 - 1
+                    Dim checkpos = New List(Of (x As Integer, y As Integer)) From {
+                        (pos.x - 1, pos.y - 1),
+                        (pos.x + 0, pos.y - 1),
+                        (pos.x + 1, pos.y - 1),
+                        (pos.x - 1, pos.y + 0),
+                        (pos.x + 1, pos.y + 0),
+                        (pos.x - 1, pos.y + 1),
+                        (pos.x + 0, pos.y + 1),
+                        (pos.x + 1, pos.y + 1)
+                    }
                     Dim neighbours = 0
                     For Each cp In checkpos
                         If field.ContainsKey(cp) Then
@@ -44,24 +44,24 @@ Module Program
                         stayed += 1
                         Exit For
                     End If
-                    checkpos = new List(Of (x As Integer, y As Integer))()
-                    Select (i+j) Mod 4
+                    checkpos = New List(Of (x As Integer, y As Integer))()
+                    Select Case (i + j) Mod 4
                         Case 0 ' north
-                            checkpos.Add((pos.x-1, pos.y-1))
-                            checkpos.Add((pos.x+0, pos.y-1))
-                            checkpos.Add((pos.x+1, pos.y-1))                            
+                            checkpos.Add((pos.x - 1, pos.y - 1))
+                            checkpos.Add((pos.x + 0, pos.y - 1))
+                            checkpos.Add((pos.x + 1, pos.y - 1))
                         Case 1 ' south
-                            checkpos.Add((pos.x-1, pos.y+1))
-                            checkpos.Add((pos.x+0, pos.y+1))
-                            checkpos.Add((pos.x+1, pos.y+1))                            
+                            checkpos.Add((pos.x - 1, pos.y + 1))
+                            checkpos.Add((pos.x + 0, pos.y + 1))
+                            checkpos.Add((pos.x + 1, pos.y + 1))
                         Case 2 ' west
-                            checkpos.Add((pos.x-1, pos.y-1))
-                            checkpos.Add((pos.x-1, pos.y+0))
-                            checkpos.Add((pos.x-1, pos.y+1))
+                            checkpos.Add((pos.x - 1, pos.y - 1))
+                            checkpos.Add((pos.x - 1, pos.y + 0))
+                            checkpos.Add((pos.x - 1, pos.y + 1))
                         Case 3 ' east
-                            checkpos.Add((pos.x+1, pos.y-1))
-                            checkpos.Add((pos.x+1, pos.y+0))
-                            checkpos.Add((pos.x+1, pos.y+1))
+                            checkpos.Add((pos.x + 1, pos.y - 1))
+                            checkpos.Add((pos.x + 1, pos.y + 0))
+                            checkpos.Add((pos.x + 1, pos.y + 1))
                     End Select
                     neighbours = 0
                     For Each cp In checkpos
@@ -75,12 +75,12 @@ Module Program
                     End If
                 Next j
                 If Not moves.ContainsKey(newpos) Then
-                    moves(newpos) = new List(Of (x As Integer, y As Integer))
+                    moves(newpos) = New List(Of (x As Integer, y As Integer))
                 End If
                 moves(newpos).Add(pos)
             Next
             If stayed = field.Count Then
-                round = i+1
+                round = i + 1
                 Exit For
             End If
             Dim newfield = New Dictionary(Of (x As Integer, y As Integer), Boolean)()
@@ -90,7 +90,7 @@ Module Program
                 Else
                     For Each prevpos In kv.Value
                         newfield(prevpos) = True
-                    Next                    
+                    Next
                 End If
             Next
             field = newfield
